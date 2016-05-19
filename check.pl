7c07e5f6e68c056333512843cf81000367ce16f8
Runar Buvik <runarb@gmail.com>
2016-05-19 10:43:32 +0200

    Better visualization when there was no errors

--- check.pl
+++ check.pl
@@ -3,6 +3,7 @@
 use strict;
 use warnings;
 use Scalar::Util qw(looks_like_number);
+use Term::ANSIColor;
 
 my @files = ({ name => "Adult_words_list_basic.txt", occurrence => 1},	{ name => "Adult_words_list_phrases.txt", occurrence => 2 });
 
@@ -14,6 +15,7 @@ foreach my $file (@files) {
 
 	my %words;
 	my $line = 0;
+	my $errors = 0;
 	while (<INF>) {
 		chomp;
 
@@ -24,11 +26,13 @@ foreach my $file (@files) {
 
 		if ($_ =~ / $/) {
 			print "\tEntry '$_' ends in a space on line $line\n";
+			$errors++;
 			next;
 		}
 
 		if ($_ =~ /  /) {
 			print "\tEntry '$_' has sequential spaces on line $line\n";
+			$errors++;
 			next;
 		}
 
@@ -36,21 +40,25 @@ foreach my $file (@files) {
 
 		if ((scalar split(' ', $word)) != $file->{occurrence}) {
 			print "\tEntry '$_' dos not have the correct number of words on line $line\n";
+			$errors++;
 			next;
 		}
 
 		if (!$value) {
 			print "\tCan not find number in entry '$_' on line $line\n";
+			$errors++;
 			next;
 		}
 
 		if (!looks_like_number($value)) {
 			print "\tValue '$value' is not a number in entry '$_' on line $line\n";
+			$errors++;
 			next;
 		}
 
 		if ($words{$word}) {
 			print "\tDuplicate word '$word' on line $line\n";
+			$errors++;
 		}
 
 		$words{$word} = 1;
@@ -61,5 +69,11 @@ foreach my $file (@files) {
 
 	close INF;
 
-	print "\n\n";
+	if ($errors == 0) {
+		print color("green"), "Ok!\n\n", color("reset");
+	}
+	else {
+		print "\n\n";
+	}
+
 }
